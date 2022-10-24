ae65b64392bc6bb873794d73e0bd66659c3f18e6
Aryan-Rajoria <57455619+Aryan-Rajoria@users.noreply.github.com>
2022-10-24 18:45:54 +0000

    adding evadb and eva connection code

--- label_studio_ml/model/eva.py
+++ label_studio_ml/model/eva.py
@@ -1,4 +1,7 @@
-
+import pandas as pd
+import os
+import subprocess
+import time
 
 from eva.server.db_api import connect
 from label_studio_ml.model import LabelStudioMLBase
@@ -11,7 +14,26 @@ from urllib.parse import urlparse
 
 class EVADBModel(LabelStudioMLBase):
 
-    def __init__():
+    def __init__(self, image_dir=None, score_threshold=0.3, device='cuda', **kwargs):
+
+        subprocess.run(['eva_server'])
+        time.sleep(10)
+
+        nest_asyncio.apply()
+        connection = connect(host='127.0.0.1', port=5432)
+        self.cursor = connection.cursor()
+
+        self.from_name, info = list(self.parsed_label_config.items())[0]
+        self.to_name = info['to_name'][0]
+
+        schema = list(self.parsed_label_config.values())[0]
+
+        self.labels_attrs = schema.get('labels_attrs')
+        if self.labels_attrs:
+            for label_name, label_attrs in self.labels_attrs.items():
+                for predicted_value in label_attrs.get('predicted_values', '').split(','):
+                    self.label_map[predicted_value] = label_name
+
         # some code to initialize EVA DB server
         pass
 
@@ -32,4 +54,49 @@ class EVADBModel(LabelStudioMLBase):
                 logger.warning(f'Can\'t generate presigned URL for {image_url}. Reason: {exc}')
         return image_url
 
-    def predict():
+    def predict(self, tasks, *kwargs):
+        # assert len(tasks) == 1 (used in LS ML code)
+        task = tasks[0]
+        image_url = self._get_image_url(task)
+        image_path = self.get_local_path(image_url)
+        # TODO EVA result Pandas should be converted to the correct format
+        self.cursor.execute('SELECT id, data, from myvideo')
+        response = self.cursor.fetch_all()
+        model_results = response.batch.frames
+        # model_results = inference_detector(self.model, image_path)
+        results = []
+        all_scores = []
+        img_width, img_height = get_image_size(image_path)
+        for bboxes, label in zip(model_results, self.model.CLASSES):
+            output_label = self.label_map.get(label, label)
+
+            if output_label not in self.labels_in_config:
+                print(output_label + ' label not found in project config.')
+                continue
+            for bbox in bboxes:
+                bbox = list(bbox)
+                if not bbox:
+                    continue
+                score = float(bbox[-1])
+                if score < self.score_thresh:
+                    continue
+                x, y, xmax, ymax = bbox[:4]
+                results.append({
+                    'from_name': self.from_name,
+                    'to_name': self.to_name,
+                    'type': 'rectanglelabels',
+                    'value': {
+                        'rectanglelabels': [output_label],
+                        'x': x / img_width * 100,
+                        'y': y / img_height * 100,
+                        'width': (xmax - x) / img_width * 100,
+                        'height': (ymax - y) / img_height * 100
+                    },
+                    'score': score
+                })
+                all_scores.append(score)
+        avg_score = sum(all_scores) / max(len(all_scores), 1)
+        return [{
+            'result': results,
+            'score': avg_score
+        }]
